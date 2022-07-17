0ee71149965a229530ac6f0348891d06effb0a2f
HamiltonLopes <hamilton_dino02@hotmail.com>
2022-07-17 15:56:49 -0300

    correct res.json

--- src/app/controllers/RewardsController.js
+++ src/app/controllers/RewardsController.js
@@ -17,7 +17,7 @@ export default new class RewardsController {
             return res.status(200).json({ "Config": "Successful" }); //retorna um status 200 para informar que a configuração foi bem sucedida
 
         if (!State) //verifica se a váriavel estado foi passada no body da requisição
-            return res.status(400).error({ "error": "Bad Request" }); //envia um erro informando requisição inválida
+            return res.status(400).json({ "error": "Bad Request" }); //envia um erro informando requisição inválida
         else if (State === "payment-approved") { //verifica se o estado do pedido é de pagamento aprovado 
             let orderResponse = await axios.get(`https://${process.env.ACCOUNT_NAME}.${process.env.ENVIROMENT}.com/api/oms/pvt/orders/${OrderId}`,
                 {headers:{"X-VTEX-API-AppKey": process.env.X_VTEX_API_AppKey, "X-VTEX-API-AppToken": process.env.X_VTEX_API_AppToken}}); //getOrderById captura informação do pedido pelo número do pedido
